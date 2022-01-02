c27b700c5e2edbbfcc6b91cbe47c8a5ac6d48f85
Eric_Lian <git@superexercisebook.com>
2022-01-02 21:10:49 +0800

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Sarasa Gothic 资源包
+
+用来让 ModernUI 能够通过资源包的形式载入 Sarasa Gothic 字体的一个模组。
+
+ModernUI 字体设置：`"sarasaembedded:font/sarasa-mono-slab-sc-semibold.ttf"`
---
+++ src/main/java/party/para/sarasaembedded/SarasaEmbedded.java
@@ -0,0 +1,13 @@
+package party.para.sarasaembedded;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("sarasaembedded")
+public class SarasaEmbedded {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public SarasaEmbedded() {
+    }
+}
